4e595c891ed15b0cbdcb0f7b2c79a11220d67d2b
DharshanPalani <dharshanpalani.2006@gmail.com>
2026-01-30 22:28:22 +0530

    Hero countdown changes and more changes

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -5,9 +5,6 @@ export const Events: React.FC = () => {
   return (
     <div className="pt-32  px-6 lg:px-20 animate-fade-in min-h-screen">
       <div className="text-center mb-3 uppercase">
-        <p className="text-white/40 text-sm mb-4   tracking-[0.5em] font-bold uppercase">
-          Events
-        </p>
         <h1 className="text-5xl md:text-7xl font-hanora tracking-tight text-white">
           Showcase <span className="font-hanora gold-text">Events</span>
         </h1>
