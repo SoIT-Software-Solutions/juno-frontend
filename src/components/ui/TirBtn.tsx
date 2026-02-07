25017b3f679983f46997b2c3e817196fa9ee9cab
DharshanPalani <dharshanpalani.2006@gmail.com>
2026-02-07 20:34:35 +0530

    button alighment change

--- src/components/ui/TirBtn.tsx
+++ src/components/ui/TirBtn.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-interface TirBtnProps {
+interface TriBtnProps {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
   type?: "button" | "submit" | "reset";
 }
 
-export const TriBtn: React.FC<TirBtnProps> = ({
+export const TriBtn: React.FC<TriBtnProps> = ({
   children,
   onClick,
   className = "",
@@ -17,7 +17,7 @@ export const TriBtn: React.FC<TirBtnProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className={`rounded-t-[20px] backdrop-blur-[29px] px-10 py-5 group relative inline-block text-white text-[12px] md:text-sm tracking-[0.4em] uppercase font-black transition-colors duration-300 hover:text-yellow-500 ${className}`}
+      className={`rounded-t-[20px] backdrop-blur-[29px] px-10 py-5 block text-center text-white text-[12px] md:text-sm tracking-[0.4em] uppercase font-black transition-colors duration-300 hover:text-yellow-500 ${className}`}
     >
       {children}
       <span className="absolute -bottom-0 left-0 w-full h-0.5 bg-yellow-500/50 scale-x-100 group-hover:bg-yellow-500 transition-all duration-300"></span>
